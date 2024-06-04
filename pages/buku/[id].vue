01cedf0ec854cdafdefe55f0ece65ab828c9a132
elsanugraha224 <152845570+elsanugraha224@users.noreply.github.com>
2024-06-04 10:26:30 +0700

    [id].vue

--- pages/buku/[id].vue
+++ pages/buku/[id].vue
@@ -5,17 +5,17 @@
             <div class="col-md-3">
                 <div class="card">
                     <div class="card-body">
-                        <span v-if="buku.cover"><img :src="buku.cover" :alt="buku.judul"></span>
+                        <span v-if="buku.cover"><img :src="buku.cover" :alt="buku.judul" width="340" height="500"></span>
                 <span v-else><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgrepo.com%2Fsvg%2F508699%2Flandscape-placeholder&psig=AOvVaw2-SWmfk33NzXubPfqn0P16&ust=1714794757874000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjln7nK8IUDFQAAAAAdAAAAABAE://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgrepo.com%2Fsvg%2F508699%2Flandscape-placeholder&psig=AOvVaw2-SWmfk33NzXubPfqn0P16&ust=1714794757874000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjln7nK8IUDFQAAAAAdAAAAABAE"></span>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">penulis : {{  buku.penulis }}</li>
-                    <li class="list-group-item">tahun_terbit : {{  buku.tahun_terbit }}</li>
-                    <li class="list-group-item">rak : {{ buku.rak }}</li>
-                    <li class=" list-group-item">deskripsi : {{ buku.deskripsi }}</li>
+                    <li class="list-group-item">penulis : {{  buku.Penulis }}</li>
+                    <li class="list-group-item">tahun_terbit : {{  buku.Tahun_Terbit }}</li>
+                    <li class="list-group-item">rak : {{ buku.Rak }}</li>
+                    <li class=" list-group-item">deskripsi : {{ buku.Deskripsi }}</li>
                 </ul>
             </div>
         </div>
@@ -33,7 +33,7 @@ const route = useRoute()
 const buku = ref([])
 
 const getBukuByID = async () => {
-    const { data, error } = await supabase.from('buku').select(', kategori()')
+    const { data, error } = await supabase.from('Buku').select(`*, kategori_buku(*)`)
     .eq('id', route.params.id)
     if(data) buku.value = data[0]
 }
